07ac6fbdc43af1f32a93f7ec2aeac365d66f389e
naveen5229 <nav1995gupta@gmail.com>
2023-03-02 15:51:44 +0530

    build workflow added for react

--- event-booking-server/src/app.js
+++ event-booking-server/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const app = express();
+const path = require('path');
 
 const { graphqlHTTP } = require('express-graphql');
 
@@ -12,40 +13,42 @@ const { Strategy } = require('passport-google-oauth20');
 
 const cookieSession = require('cookie-session');
 
-// passport.use(new Strategy({
-//     clientID: process.env.CLIENT_ID,
-//     clientSecret: process.env.CLIENT_SECRET,
-//     callbackURL: 'http://localhost:5000/auth/google/verify'
-// },
-//     (accessToken, refreshToken, profile, done) => {
-//         console.log(profile)
-//         done(null, profile);
-//     }));
+app.use(express.static(path.join(__dirname, './public')));
 
-//     passport.serializeUser((user, done) => {
-//         done(null, user.id);
-//     });
+passport.use(new Strategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: 'http://localhost:5000/auth/google/verify'
+},
+    (accessToken, refreshToken, profile, done) => {
+        console.log(profile)
+        done(null, profile);
+    }));
 
-//     passport.deserializeUser((id, done) => {
-//         done(null, id);
-//     });
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser((id, done) => {
+        done(null, id);
+    });
 
 const CORS = require('cors');
+const { join } = require('path');
 
 app.use(CORS({
     origin: `http://localhost:3000`
 }));
 
-// app.use(cookieSession({
-//     name:'session',
-//     maxAge: 24 * 60 * 60 * 1000,
-//     keys: [process.env.SESSION_KEY]
-// }))
+app.use(cookieSession({
+    name:'session',
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [process.env.SESSION_KEY]
+}))
 
 app.use(require('./middleware/auth.middle'));
-// app.use(passport.initialize());
-// app.use(passport.session())
-
+app.use(passport.initialize());
+app.use(passport.session())
 
 app.use('/graphql', graphqlHTTP({
     schema: graphqlSchema,
@@ -53,24 +56,29 @@ app.use('/graphql', graphqlHTTP({
     graphiql: true
 }));
 
-// app.get('/', passport.authenticate('google', {
-//     scope: ['email']
-// }), (req, res) => {
+app.get('/auth', passport.authenticate('google', {
+    scope: ['email']
+}), (req, res) => {
     
-// });
+});
 
 
-// app.get('/auth/google/verify', passport.authenticate('google', {
-//     failureRedirect: '/failure',
-//     successRedirect:'/graphql',
-//     session:true
-// }), (req, res) => {                                 //req.user
-//     console.log(req.cookies)
-// });
+app.get('/auth/google/verify', passport.authenticate('google', {
+    failureRedirect: '/failure',
+    successRedirect:'/graphql',
+    session:true
+}), (req, res) => {                                 //req.user
+    console.log(req.cookies)
+});
 
 app.get('/test', (req, res) => {
     res.status(201).send('test');
 })
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname , './public /index.html'));
+});
+
 
 
 
